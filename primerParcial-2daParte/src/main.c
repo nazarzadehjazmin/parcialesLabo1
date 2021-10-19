f859180139444815bfa6edd1487ff5801e3a869c
nazarzadehjazmin <nazarzadehjazmin@gmail.com>
2021-10-19 20:35:59 -0300

    f

--- primerParcial-2daParte/src/main.c
+++ primerParcial-2daParte/src/main.c
@@ -17,7 +17,7 @@ int main(void) {
 	{
 		do
 		{
-			if(utn_getNumero(&menuOption, "\n\nMenu principal:\n1-Alta cliente \n2-Modificar cliente \n3-Baja cliente \n4-Crear pedido de recoleccion \n5-Procesar residuos \n6-Imprimir clientes \n7-Imprimir pedidos pendientes \n8-Imprimir pedidos procesados \n9-Cantidad de pedidos pendientes por localidad \n10-Cantidad de kg de PP promedio por cliente \n11- mas pendientes \n12-mas completos \n13-Salir \n", ERROR_MSG, 1, 11, QTY_REINTENTO) == 0)
+			if(utn_getNumero(&menuOption, "\n\nMenu principal:\n1-Alta cliente \n2-Modificar cliente \n3-Baja cliente \n4-Crear pedido de recoleccion \n5-Procesar residuos \n6-Imprimir clientes \n7-Imprimir pedidos pendientes \n8-Imprimir pedidos procesados \n9-Cantidad de pedidos pendientes por localidad \n10-Cantidad de kg de PP promedio por cliente \n11- mas pendientes \n12-mas completos \n13-Salir \n", ERROR_MSG, 1, 13, QTY_REINTENTO) == 0)
 			{
 				switch(menuOption)
 				{
